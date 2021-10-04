ee6c196528a095849cec9048d86033456e66045b
Julien <julienfroger16@gmail.com>
2021-10-04 14:21:43 +0200

    Add: charts in 'Information complémentaire'

--- src/components/Other.js
+++ src/components/Other.js
@@ -8,6 +8,27 @@ const Other = () => {
                 <div className="icon-title">{ iconInfoSupp }</div>
                 <h1>informations complémentaires</h1>
             </div>
+            <div className="charts-group">
+            <div className="charts chart-photoshop">
+                <h2>Adobe Photoshop</h2>
+            </div>
+            <div className="charts chart-word">
+                <h2>Microsoft Word</h2>
+            </div>
+            <div className="charts chart-google">
+                <h2>Google ads</h2>
+            </div>
+            <div className="charts chart-acrobat">
+                <h2>adobe acrobat reader</h2>
+            </div>
+            <div className="charts chart-excel">
+                <h2>Microsoft excel</h2>
+            </div>
+            <div className="charts chart-vscode">
+                <h2>visual studio code</h2>
+            </div>
+
+            </div>
         </div>
     );
 };
