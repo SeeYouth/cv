dac3700b35c98ddb0aa5a44bde64134a11c8ed35
Julien <julienfroger16@gmail.com>
2021-10-20 13:12:24 +0200

    add: Formation to date

--- src/components/Education.js
+++ src/components/Education.js
@@ -12,6 +12,15 @@ const Education = () => {
                 <ul>
                     <div className="subtitle">
                         <div className="icon-subtitle"></div>
+                        <h2><span>2019 - À aujourd'hui</span>formation en autodidacte</h2>
+                    </div>
+                    <li>Maîtrise du langage HTML et CSS</li>
+                    <li>Confirmation des connaissances en Javascript </li>
+                    <li>Découverte de nouvelle technologie - REACT</li>
+                </ul>
+                <ul>
+                    <div className="subtitle">
+                        <div className="icon-subtitle"></div>
                         <h2><span>avril-octobre 2019</span>openclassrooms</h2>
                     </div>
                     <li>Développement d’une interface utilisateur</li>
